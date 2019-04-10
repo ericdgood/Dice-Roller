f36c50fb09e40eb7da05d67973d77a539f297d12
Eric Good <ericdgood@yahoo.com>
2019-04-09 23:18:27 -0400

    Complete

--- app/src/main/java/example/pia/diceroller/MainActivity.kt
+++ app/src/main/java/example/pia/diceroller/MainActivity.kt
@@ -1,12 +1,36 @@
 package example.pia.diceroller
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var randomRoll: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnRoll.setOnClickListener {
+            rollDice()
+        Toast.makeText(this, "You rolled a $randomRoll", Toast.LENGTH_SHORT).show()}
     }
+
+     private fun rollDice() {
+     randomRoll = (Random().nextInt(6) + 1)
+
+         val diceImage =  when(randomRoll){
+             1 -> R.drawable.dice_1
+             2 -> R.drawable.dice_2
+             3 -> R.drawable.dice_3
+             4 -> R.drawable.dice_4
+             5 -> R.drawable.dice_5
+             else -> R.drawable.dice_6
+         }
+
+         imgDice.setImageResource(diceImage)
+     }
 }
---
+++ app/src/test/java/example/pia/diceroller/MainActivityTest.kt
@@ -0,0 +1,8 @@
+package example.pia.diceroller
+
+import org.junit.Assert.*
+
+/**
+ * Created by Eric on 4/9/2019.
+ */
+class MainActivityTest
